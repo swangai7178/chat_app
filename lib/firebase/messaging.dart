7cb4a0ec4965af6a711cbe24d6ce1a26bb7ed0dc
samuel wangai <44896330+swangai7178@users.noreply.github.com>
2022-04-25 11:26:01 +0300

    l

--- lib/firebase/messaging.dart
+++ lib/firebase/messaging.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
